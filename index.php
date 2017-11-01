edf96769f1b5ac444b5c622fea385df539f4f811
samit2011 <32530155+samit2011@users.noreply.github.com>
2017-11-01 15:45:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php include 'header.php'; ?>
 <h1>Homepage</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Nullam volutpat elementum purus lobortis tempor.
-Mauris augue magna, facilisis at rhoncus at, pulvinar eu sem.
-Donec imperdiet volutpat tortor id efficitur.
-Curabitur in ante sit amet odio semper condimentum.
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Nullam volutpat elementum purus lobortis tempor.
-Mauris augue magna, facilisis at rhoncus at, pulvinar eu sem.
-Donec imperdiet volutpat tortor id efficitur.
-Curabitur in ante sit amet odio semper condimentum.
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Nullam volutpat elementum purus lobortis tempor.
-Mauris augue magna, facilisis at rhoncus at, pulvinar eu sem.
-Donec imperdiet volutpat tortor id efficitur.
-Curabitur in ante sit amet odio semper condimentum.</p>
+<p>MyCarDatabase is a Web Application built for vehicle dealerships to keep track of their vehicle inventory. 
+   MyCarDatabase will also serve as a tool for dealerships to use to aid customers in purchasing a vehicle
+   based on the customer's criteria.  MyCarDatabase will also serve as a dealership management system,
+   accounting for basic account information, service options, financing options, warranty options.</p>
 <?php include 'footer.php'; ?>
