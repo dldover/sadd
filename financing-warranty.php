9026f1ba41e08cb372d83177d578a4440ec2912f
daniyalf <33036703+daniyalf@users.noreply.github.com>
2017-11-01 16:12:55 -0400

    Update financing-warranty.php

--- financing-warranty.php
+++ financing-warranty.php
@@ -1,4 +1,11 @@
 <?php include 'header.php'; ?>
 <h1>Financing / Warranty</h1>
-<p>Financing and warranty information goes here.</p>
+<p><pre>Finance options: 
+        4.5% interest rate for 60 months
+        5.5% interest rate for 72 months
+        7.9% interest rate for 120 months
+    Warranty options:
+      Every car comes with a 6 month or 36,000 miles warranty.
+      For additional $3,000, get 10 month or 100,000 mile warranty.
+      For additional $5,000, get 1 year or 120,000 mile warranty.</pre></p>
 <?php include 'footer.php'; ?>
