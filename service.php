1f63116f1a660c93e256dcd7afea1272f0959314
samit2011 <32530155+samit2011@users.noreply.github.com>
2017-11-01 16:09:32 -0400

    Update service.php

--- service.php
+++ service.php
@@ -1,11 +1,20 @@
 <?php include 'header.php'; ?>
 <h1>Services</h1>
-<p>        Basic Oil Change:..............................$29.99
+<p>  
+<pre>      Basic Oil Change:..............................$29.99
      High Milage Oil Change:..............................$49.99
 Tire Alignment And Rotation:..............................$79.99
       Brake Pad Replacement:..............................$89.99
           Rotor Replacement:.............................$119.99
  Air Filtration and Cooling:..............................$69.99
         Battery Replacement:..............................$99.99
-       Fuel System Cleaning:..............................$49.99</p>
+       Fuel System Cleaning:..............................$49.99
+     
+     See service mechanic if vehicle needs: Engine Services, Transmission Services,
+     DriveTrain Services, Cooling System Services, Emission and Inspection Services,
+     Inspection Services, Exhaust Services, Exterior Body Services.  The quote will
+     be given upon inspection of the vehicle.
+     
+</pre>               Customer must call 227-328-2227 for services
+</p>
 <?php include 'footer.php'; ?>
