929d1cdc9f6569b386b26706b10774df2a58daf7
ammarakhalid95 <32373280+ammarakhalid95@users.noreply.github.com>
2017-11-01 16:12:58 -0400

    Update about.php

--- about.php
+++ about.php
@@ -1,4 +1,30 @@
 <?php include 'header.php'; ?>
 <h1>About Us</h1>
-<p>About us information goes here.</p>
+<p>We know it's easy to misplace paper documents with all the important information. 
+We know sometimes it's hard to mark off a vehicle you sold, as well as using multiple
+  search engines to compare cards. Here at SADD Inc., we created an online database because we understand the need
+  to store all vehicle information in one place. 
+  
+  We know our database isn't perfect. If you have any comments, concerns, questions, or suggestions on how to improve our database, 
+  please see our contact information below:
+
+
+</p>
+//Contact information should be the left-positioned
+
+<p> 
+Phone Number:
+  227-328-2227
+</p>
+
+<p> 
+Phone Number:
+ saddinc@gmail.com
+</p>
+
+<p> 
+Physical Address:
+ 7233 Lewis Street, Silver Spring, Maryland 20815
+</p>
+
 <?php include 'footer.php'; ?>
