a90d88c39924424249d8763a4985096872ac3e49
samit2011 <32530155+samit2011@users.noreply.github.com>
2017-11-01 15:59:47 -0400

    Update service.php

--- service.php
+++ service.php
@@ -1,4 +1,11 @@
 <?php include 'header.php'; ?>
-<h1>Service</h1>
-<p>Service information goes here.</p>
+<h1>Services</h1>
+<p>        Basic Oil Change:..............................$29.99
+     High Milage Oil Change:..............................$49.99
+Tire Alignment And Rotation:..............................$79.99
+      Brake Pad Replacement:..............................$89.99
+          Rotor Replacement:.............................$119.99
+ Air Filtration and Cooling:..............................$69.99
+        Battery Replacement:..............................$99.99
+       Fuel System Cleaning:..............................$49.99</p>
 <?php include 'footer.php'; ?>
