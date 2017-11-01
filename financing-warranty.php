75ab8e99e8c2fb7fecf8648a757cf8fc2da5983c
daniyalf <33036703+daniyalf@users.noreply.github.com>
2017-11-01 17:04:52 -0400

    Update financing-warranty.php

--- financing-warranty.php
+++ financing-warranty.php
@@ -7,9 +7,9 @@
     Warranty options:
       Every car comes with a 6 month or 36,000 miles warranty.
       For additional $3,000, get 10 month or 100,000 mile warranty.
-      For additional $5,000, get 1 year or 120,000 mile warranty.
+      For additional $5,000, get 1 year or 120,000 mile warranty.</pre>
 Warranty does not include regular car maintence services. When the month or miles exceed the limit, 
 the warranty will expire without notice. The warranty covers everything in the car including power train coverage,
 air conditioning, audio system, and electrical components. Roadside asistance is not covered in warranty.
-For further information see owners manual.</pre></p>
+For further information see owners manual.</p>
 <?php include 'footer.php'; ?>
